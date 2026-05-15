38b47940c8005c5fa50aba2915e2439f84a351d5
temijopelo <temijeje@gmail.com>
2026-05-15 11:56:40 +0100

    chore: update Node engine to 20.x for Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,8 @@ import vercel from "@sveltejs/adapter-vercel";
 const config = {
   kit: {
     adapter: vercel({
-      // force a Node 18 runtime on Vercel
-      runtime: "nodejs18.x",
+      // force a Node 20 runtime on Vercel
+      runtime: "nodejs20.x",
     }),
   },
 };
